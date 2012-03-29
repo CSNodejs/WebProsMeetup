3503ce31d769cc9c7ebee1cefba7a2558d4814a4
Jerad Bitner <sirkitree@gmail.com>
2012-03-28 19:05:50 -0600

    making this a little more reasonable

--- pcap-map/public/javascripts/map.js
+++ pcap-map/public/javascripts/map.js
@@ -29,7 +29,7 @@ socket.on('newPoint', function (data) {
         fillOpacity: 0.5
     };
 
-  var circle = new L.Circle(circleLocation, 500000, circleOptions);
+  var circle = new L.Circle(circleLocation, 5000, circleOptions);
   map.addLayer(circle);
   map.setView(circleLocation, 8);
 });
