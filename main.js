c226f769c908cdc9a433053da47ea7e8634e4340
Jerad Bitner <sirkitree@gmail.com>
2012-04-11 20:18:14 -0600

    Adding two more listeners.

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ server = http.createServer(function(request, response) {
     //parse the request url for the path and grab the filename
     var uri = url.parse(request.url).pathname
     , filename = path.join(process.cwd(), uri);
-  
+
   //if filename doesnt exist show 404
     path.exists(filename, function(exists) {
         if(!exists) {
@@ -22,11 +22,11 @@ server = http.createServer(function(request, response) {
             response.end();
             return;
         }
-    
+
     //read the filename
         fs.readFile(filename, "binary", function(err, file) {
             //if an error occured reading show a 500 error on the page
-            if(err) {        
+            if(err) {
                 response.writeHead(500, {
                     "Content-Type": "text/plain"
                 });
@@ -36,7 +36,7 @@ server = http.createServer(function(request, response) {
             }
             //check to see if the file is a .js file
             var filematch = new RegExp(/(js$)/);
-            
+
             //if file is a javascript file send the file content-type as javascript otherwise its sent as text/plain
             if(filename.match(filematch)){
                 response.writeHead(200, {
@@ -49,7 +49,7 @@ server = http.createServer(function(request, response) {
                 response.write(file, "binary");
                 response.end();
             }
-      
+
         });
     });
 }).listen(parseInt(port, 10));
@@ -57,20 +57,28 @@ server = http.createServer(function(request, response) {
 //init socket.io
 var sio = io.listen(server);
 sio.sockets.on("connection", function(socket){
-    
+
     socket.on("newmessage", function(msg){
        socket.broadcast.emit("chatmessage", {
            text: msg.text,
            user: msg.user
-       }) 
+       })
     });
-    
+
+    socket.on("newUser", function(data){
+        listView.addUserList(data);
+    });
+
+    socket.on("deleteUser", function(data){
+        listView.removeUserList(data);
+    });
+
     setTimeout(function() {
         socket.broadcast.emit('chatmessage', {
             text: "New user has entered chat."
         });
     }, 3000);
-    
+
 });
 
 console.log("Chat server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
