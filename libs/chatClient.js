e69e03824c0ce4c6d959a5068b2b1f2b7d2e85bc
John Ferris <john@pixel-whip.com>
2012-04-11 20:50:44 -0600

    Wrapped message in spans.

--- libs/chatClient.js
+++ libs/chatClient.js
@@ -1,14 +1,14 @@
 (function($){
-    
+
     var socket = io.connect("http://173.8.248.29/");
-    
+
     var Item = Backbone.Model.extend({
         defaults: {
             part1: 'hello',
             part2: 'world'
         }
-    });      
-  
+    });
+
     var List = Backbone.Collection.extend({
         model: Item
     });
@@ -18,20 +18,20 @@
         events: {
             'click button#add': 'addItem'
         },
-    
+
         initialize: function(){
             _.bindAll(this, 'render', 'addItem', 'appendItem');
-      
+
             this.collection = new List();
-            this.collection.bind('add', this.appendItem); 
+            this.collection.bind('add', this.appendItem);
 
             this.counter = 0;
-            this.render();      
+            this.render();
         },
         render: function(){
-            
+
             var self = this,
-            
+
             button = $("<button/>", {
                 id : "add"
             }).html("Send"),
@@ -45,35 +45,35 @@
                 style : "border-color: black; border-width: 1px; border-style: solid;"
             }),
             UserList = $("<ul/>", {
-                id : "userlist"                
+                id : "userlist"
             }),
             textarea = $("<input/>", {
                 id : "chatentry",
                 width : 200
             });
-            
+
             $("#main").append(container);
             $(this.el).append(nicktext);
             $(this.el).append(textarea);
             $(this.el).append(button);
             $("#aside").append(UserList);
-           
-            _(this.collection.models).each(function(item){ 
+
+            _(this.collection.models).each(function(item){
                 self.appendItem(item);
             }, this);
         },
-        
+
         addItem: function(){
             var item = new Item(),
             $chat = $('#chatentry'),
             $nick = $('#nickentry');
-            
+
             item.set('chattext', $chat.val());
             item.set('nicktext', $nick.val());
-            
+
             $chat.val("");
             $nick.hide();
-            
+
             socket.emit("newmessage", {
                 "text" : item.get('chattext'),
                 "user" : item.get('nicktext')
@@ -82,41 +82,41 @@
         },
         addItemSoc: function(data){
             var item = new Item();
-            
+
             item.set('chattext', data.text);
             item.set('nicktext', data.user);
-            
+
             this.collection.add(item);
         },
         addUserList: function(data){
             var item = new Item();
             item.set("nick", data.nick);
-            
-            $("#userlist").append("<li>" + item.get('nick') + "</li>");
+
+            $('#userlist').append('<li class="user">' + item.get('nick') + '</li>');
             this.collection.add(item);
         },
         removeUserList: function(data){
-            
+
             //$('ul', this.el).append("<li>" + item.get('nicktext') + ":" + item.get('chattext') + "</li>");
         },
         appendItem: function(item){
-            $('ul', this.el).append("<li>" + item.get('nicktext') + ":" + item.get('chattext') + "</li>");
+            $('ul', this.el).append('<li><span class="username">' + item.get('nicktext') + '</span>: <span class="message">' + item.get('chattext') + '</span></li>');
         }
     });
 
     var listView = new ListView();
     console.log(listView);
-    
+
     socket.on("chatmessage", function(data){
         listView.addItemSoc(data);
     });
-    
+
     socket.on("newUser", function(data){
         listView.addUserList(data);
     });
-    
+
     socket.on("deleteUser", function(data){
-       listView.removeUserList(data); 
+       listView.removeUserList(data);
     });
-    
+
 })(jQuery);
